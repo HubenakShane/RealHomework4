3212c0067d0ed9756445120ffee69b80ea58065c
hubenakshane <hubenakshane@gmail.com>
2021-09-16 20:47:48 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("Script is running")
+
+var startQuizButton = document.getElementById("start-quiz");
+
+function startQuiz() {
+    document.getElementById('quiz').classList.remove("hidden")
+}
+
+startQuizButton.addEventListener("click", startQuiz)
+
+Var startHighscoreButton = document.getElementById("Highscore");
+
+.
